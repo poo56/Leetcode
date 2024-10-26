17baf0cdc7c49f216b75f34614fbbd589e2b1b76
poo56 <102963584+poo56@users.noreply.github.com>
2024-10-26 19:02:23 -0400

    Add files via upload

---
+++ Two pointers/392.Is Sequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_len = len(s)
+        t_len = len(t)
+
+        i = 0
+        j = 0
+        while i < s_len and j < t_len:
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == s_len
